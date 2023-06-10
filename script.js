3114b5bc18624fce7c95d32cc2c6520118b38560
Sevan Kelpentidjian <s.kelpet@gmail.com>
2023-06-10 14:00:26 +0200

    modif for prod

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+//let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+const taxiFareApiUrl = 'https://taxifare-s6rfo4atlq-ew.a.run.app'
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
